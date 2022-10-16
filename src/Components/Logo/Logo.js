44c3d3746794ab15916e07d7b656b964c875e6ec
enssaf al-shwayat <enssaf_ibrahim@yahoo.com>
2022-10-17 00:47:08 +0300

    add js file

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,25 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import './Logo.css'
+import { mydata } from './mydata'
 export default function Logo() {
-    const [logo, setlogo] = useState([
-        {id: 1,
-         image: './images/logo/img0.png'
-        },
-        {id: 2,
-        image: './images/logo/img1.png'
-        },
-        {id: 3,
-        image: './images/logo/img2.png'
-        },
-        {id: 4,
-        image: './images/logo/img3.png'
-        },
-        {id: 5,
-        image: './images/logo/img4.png'
-        }
-    ])
+    const [logo, setlogo] = useState([])
 
-    
+    useEffect(()=> {
+      setlogo(mydata)
+    })
   return (
     <div className='logo-section'>
         <div className='container'>
---
+++ src/Components/Logo/mydata.js
@@ -0,0 +1,23 @@
+import img1 from '../../assests/images/logo/img0.png'
+import img2 from '../../assests/images/logo/img1.png'
+import img3 from '../../assests/images/logo/img2.png'
+import img4 from '../../assests/images/logo/img3.png'
+import img5 from '../../assests/images/logo/img4.png'
+
+export const mydata = [
+    {id: 1,
+     image: img1
+    },
+    {id: 2,
+    image: img2
+    },
+    {id: 3,
+    image: img3
+    },
+    {id: 4,
+    image: img4
+    },
+    {id: 5,
+    image: img5
+    }
+]
