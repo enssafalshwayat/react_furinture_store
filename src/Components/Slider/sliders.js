3f8c664c15b97f544052b2cedc8eba151abed0f7
enssaf al-shwayat <enssaf_ibrahim@yahoo.com>
2022-10-17 00:34:25 +0300

    change url

--- src/Components/Slider/sliders.js
+++ src/Components/Slider/sliders.js
@@ -4,7 +4,7 @@ import slide2 from '../../assests/images/slide6.jpg'
 export const sliders =
  [
     {   id: 1,
-        image: '../../../assests/images/slide2.png',
+        image: slide,
         titleS: 'Great Design Collection',
         title: 'Cloth Covered Accent Chair',
         description:'Lorem Ipsum Dolor Sit Amet, Consectetur Adipisicing Elit, Sed Do Eiuiana Smod Tempor Ut Labore Et Dolore Magna Aliqua. Ut Enim Ad Minim Veniam, Quis Nostrud Exercitation Ullamco Laboris Nisi Ut Aliquip.',
@@ -12,7 +12,7 @@ export const sliders =
         oldPrice: '$499.00'
     },
     {   id: 2,
-        image: '../../../assests/images/slide5.png',
+        image: slide1,
         titleS: 'Great Design Collection',
         title: 'Mapple Wood Accent Chair',
         description:'Lorem Ipsum Dolor Sit Amet, Consectetur Adipisicing Elit, Sed Do Eiuiana Smod Tempor Ut Labore Et Dolore Magna Aliqua. Ut Enim Ad Minim Veniam, Quis Nostrud Exercitation Ullamco Laboris Nisi Ut Aliquip.',
@@ -20,7 +20,7 @@ export const sliders =
         oldPrice: '$599.00'
     },
     {   id: 3,
-        image: '../../../assests/images/slide6.jpg',
+        image: slide2,
         titleS: 'Great Design Collection',
         title: 'Valvet Accent Arm Chair',
         description:'Lorem Ipsum Dolor Sit Amet, Consectetur Adipisicing Elit, Sed Do Eiuiana Smod Tempor Ut Labore Et Dolore Magna Aliqua. Ut Enim Ad Minim Veniam, Quis Nostrud Exercitation Ullamco Laboris Nisi Ut Aliquip.',
